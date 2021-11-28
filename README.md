64fd4be57fedae99b6697906849769304a5dbf3e
SebastianMosqueraCaicedo <1005893565@u.icesi.edu.co>
2021-11-28 09:12:58 -0500

    agrega nuevos ejercicios

--- README.md
+++ README.md
@@ -1,1 +1,145 @@
-# ts_practica
+# Ejercicios
+
+## Información relevante
+Se encuentra realizando un sistema de taller para camiones, el cual lleva el control de cuantas veces se ha hecho mantenimiento.
+
+### Camiones
+Actualmente los camiones registrados son:
+
+#### VCQ-392
+Año: 2006
+Ciudad de placa: Bogotá
+Color: Verde
+
+#### BSI-302
+Año: 2016
+Ciudad de placa: Cali
+Color: Rojo
+
+#### PQO-238
+Año: 2020
+Ciudad de placa: Medellin
+Color: Azul
+
+#### AKL-192
+Año: 1999
+Ciudad de placa: Santa Marta
+Color: Negro
+
+#### REW-392
+Año: 1996
+Ciudad de placa: Bogotá
+Color: Negro
+
+#### APM-400
+Año: 2015
+Ciudad de placa: Cali
+Color: Blanco
+
+#### GGG-969
+Año: 2003
+Ciudad de placa: Medellin
+Color: Rojo
+
+#### CAL-073
+Año: 2021
+Ciudad de placa: Santa Marta
+Color: Azul
+
+#### PRI-515
+Año: 1988
+Ciudad de placa: Cali
+Color: Azul
+
+#### MBL-403
+Año: 2000
+Ciudad de placa: Bogotá
+Color: Rojo
+
+#### Ingresos
+Estos han sido los ingresos al taller durante los últimos 5 años:
+
+##### VCQ-392
+- 2021: 1
+- 2020: 2
+- 2019: 1
+- 2018: 3
+- 2017: 0
+
+#### BSI-302
+- 2021: 2
+- 2020: 0
+- 2019: 2
+- 2018: 0
+- 2017: 1
+
+#### PQO-238
+- 2021: 1
+- 2020: 0
+- 2019: 0
+- 2018: 0
+- 2017: 0
+
+#### AKL-192
+- 2021: 2
+- 2020: 1
+- 2019: 0
+- 2018: 2
+- 2017: 1
+
+#### REW-392
+- 2021: 0
+- 2020: 2
+- 2019: 0
+- 2018: 2
+- 2017: 0
+
+#### APM-400
+- 2021: 3
+- 2020: 1
+- 2019: 1
+- 2018: 0
+- 2017: 2
+
+#### GGG-969
+- 2021: 0
+- 2020: 1
+- 2019: 0
+- 2018: 0
+- 2017: 1
+
+#### CAL-073
+- 2021: 0
+- 2020: 0
+- 2019: 0
+- 2018: 0
+- 2017: 0
+
+#### PRI-515
+- 2021: 1
+- 2020: 1
+- 2019: 1
+- 2018: 0
+- 2017: 4
+
+#### MBL-403
+- 2021: 1
+- 2020: 1
+- 2019: 1
+- 2018: 1
+- 2017: 1
+
+## Ejercicios
+Utilizando las estructuras de datos vistas en clase:
+
+1. Muestre la cantidad de ingresos al taller en el año 2018
+
+2. Genere la lista de todos los colores de camiones que han ingresado al taller
+
+3. El gobierno de la ciudad de Bogotá solicitó una lista de todos los camiones registrados en esta ciudad que hayan tenido mantenimientos realizados en 2019
+
+4. Generar un reporte de todos los camiones, junto con la cantidad de mantenimientos realizados en el año 2017
+
+5. El gobierno nacional requiere de un reporte de todos los camiones más antiguos a 2010, ordenados por su numero de placa, en el que se muestre la cantidad de mantenimientos realizados de 2021 a 2017
+
+6. El gobierno de Santa Marta, requiere un reporte de todos los camiones registrados ahí, ordenados por cantidad de mantenimientos totale
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,123 @@
-import Canvas from './canvas';
+//1. Agrega los números 4 y 5 al inicio del arreglo numbersList.
+const numbersList: number[] = [1, 2, 3];
+//Solución
+numbersList.unshift(4, 5);
 
-window.onload = function () {
-  new Canvas(800, 600, 0x0FEFEF);
+//2. Elimina el primer elemento del array fishArray
+const fishArray: string[] = ['angel', 'clown', 'mandarin', 'surgeon'];
+//Solución
+fishArray.shift();
+
+//3. Elimina el ultimo elemento del array plants
+const plants: string[] = ['broccoli', 'cauliflower', 'cabbage', 'kale', 'tomato'];
+//Solucion
+plants.pop();
+
+//4. Guarda en indeces todos los índices donde aparezca la letra "a" en el array letters
+var indices: number[] = [];
+const letters: string[] = ['a', 'b', 'a', 'c', 'a', 'd'];
+const element: string = 'a';
+
+//Solución
+
+function indicesLetters() {
+  let arrayIndices: number[] = [];
+  let contador = -1;
+  letters.forEach(function (letras) {
+    contador++;
+    if (letras === element) {
+      arrayIndices.push(contador)
+    }
+  })
+  return arrayIndices;
 }
+
+indices = indicesLetters();
+
+console.log(indices);
+
+//5. Encuentra el índice del primer número que sea menor a 13 del array1, para esto, usa la función flecha isLargeNumber
+const array1 = [5, 12, 8, 130, 44];
+const isLargeNumber = (element: number) => element > 13;
+//Solución
+const indiceMayor = array1.findIndex(isLargeNumber);
+
+//6. Junta los 3 arrays en numbersArray de manera que los números queden en orden.
+const num1: number[] = [7, 8, 9];
+const num2: number[] = [1, 2, 3];
+const num3: number[] = [4, 5, 6];
+//Solución
+
+//7. Aplicale raiz cuadrada a todos los números del arreglo nums y guarda los resultados en squareRoot
+const nums: number[] = [1, 16, 9];
+var squareRoot: number[] = [];
+//Solución
+
+//8. Ahora multiplica el valor de cada número de squareRoot por 3 y guarda los resultados en numsTimesThree
+var numsTimesThree: number[] = [];
+//Solución
+
+//9. Guarda todas las palabras de wordsArray que tengan más de 6 letras en wordsArrayResults
+const wordsArray: string[] = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+var wordsArrayResults: string[] = [];
+//Solución
+
+//10. Guarda todas los números de unfilteredNumbers que sean mayores o iguales a 10 en filteredNumbers, usa la función isLargeEnough
+const unfilteredNumbers: number[] = [12, 5, 8, 130, 44];
+var filteredNumbers: number[] = [];
+
+function isLargeEnough(num: number): boolean {
+  return num >= 10;
+}
+
+//Solución
+
+//11. Guarda los nombres del array names de la posición 1 a la 3 en maleNames
+const names: string[] = ['Rita', 'Pedro', 'Miguel', 'Juan', 'Vanesa'];
+var maleNames: string[] = [];
+//Solución
+
+//12. Usa la función printPlanets para mostrar la posición y el nombre de cada planeta del arreglo planets
+const planets: string[] = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+
+function printPlanets(planet: string, index: number, array: string[]) {
+  console.log("Planet pos: [" + index + "] = " + planet);
+}
+
+//Solución
+
+//13. Invierte el orden del array planets
+//Solución
+
+//14. ¿Los números del arreglo numbers2 son menores a 40? usa la función flecha isBelowThreshold para responder la pregunta
+const numbers2: number[] = [1, 30, 39, 29, 10, 13];
+
+const isBelowThreshold = (currentValue: number) => currentValue < 40;
+//Solución
+
+//15. Cambia todos los datos del array numbers2 por el número 4
+//Solución
+
+//16. Cambia todos los datos del array numbers2 por el número 8 desde el indice 2
+//Solución
+
+//17. Suma todos los datos del array numbers3 y guarda el resultado en la variable numbers3Result, usa la función flecha reducer
+const numbers3: number[] = [1, 2, 3, 4];
+var numbers3Result: number = 0;
+
+const reducer = (previousValue: number, currentValue: number) => previousValue + currentValue;
+//Solución
+
+//18. Comprueba si en el array numbers3 hay algún número par, usa la función felcha even
+const even = (element: number) => element % 2 === 0;
+
+//Solución
+
+//19. Comprueba si en el array fruits hay un elemento banana, usa la función flecha checkBanana
+const fruits: string[] = ['apple', 'banana', 'mango', 'guava'];
+
+const checkBanana = (element: string) => element === "banana";
+
+//Solución
+
+console.log(" 1452345");
